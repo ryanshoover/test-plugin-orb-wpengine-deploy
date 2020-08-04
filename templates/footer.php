4d6c290ad6f64d9464a8cc880a9743449610d9a1
Ryan Hoover <ryan.hoover@wpengine.com>
2020-08-03 22:44:17 -0500

    Add linting for composer

--- templates/footer.php
+++ templates/footer.php
@@ -9,7 +9,7 @@ namespace OrbTest\Plugin;
 
 ?>
 <aside>
-    <p>
-        Plugin used solely for testing functional in orb-wpengine-deploy.
-    </p>
+	<p>
+		Plugin used solely for testing functional in orb-wpengine-deploy.
+	</p>
 </aside>
