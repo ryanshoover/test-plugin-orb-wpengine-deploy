a73657958e7277baff3ac08726dfb8c8b5c21c12
Ryan Hoover <ryan.hoover@wpengine.com>
2020-08-25 22:43:55 -0500

    Remove unused backstop

--- tests/backstop/engine_scripts/puppet/onReady.js
+++
@@ -1,5 +0,0 @@
-module.exports = async( page, scenario ) => {
-	console.log( 'SCENARIO > ' + scenario.label ); // eslint-disable-line no-console
-	await require( './clickAndHoverHelper' )( page, scenario );
-	await require( './formSubmission' )( page, scenario );
-};
